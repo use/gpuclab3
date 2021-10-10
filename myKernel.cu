7e6c6b8bd85df4e974fb3396c7c667574e0af3fa
Will Hall <willhall@outlook.com>
2021-10-09 17:44:40 -0700

    Finish assignment

--- myKernel.cu
+++ myKernel.cu
@@ -13,22 +13,52 @@ __global__ void kernel( int *a, int dimx, int dimy )
 // in order to meet the requirements in the write-ups. 
 __global__ void kernel2( int *a, int dimx, int dimy )
 {
+    int ix   = blockIdx.x*blockDim.x + threadIdx.x;
+    int iy   = blockIdx.y*blockDim.y + threadIdx.y;
+    int idx = iy*dimx + ix;
+    if (ix < dimx && iy < dimy){
+        a[idx] = blockIdx.x + blockIdx.y * (dimx / blockDim.x + 1);
+    }
 }
 
 __global__ void kernel3( int *a, int dimx, int dimy )
 {
+    int ix   = blockIdx.x*blockDim.x + threadIdx.x;
+    int iy   = blockIdx.y*blockDim.y + threadIdx.y;
+    int idx = iy*dimx + ix;
+    if (ix < dimx && iy < dimy){
+        a[idx] = idx;
+    }
 }
 
 __global__ void kernel4( int *a, int dimx, int dimy )
 {
+    int ix   = blockIdx.x*blockDim.x + threadIdx.x;
+    int iy   = blockIdx.y*blockDim.y + threadIdx.y;
+    int idx = iy*dimx + ix;
+    if (ix < dimx && iy < dimy){
+        a[idx] = threadIdx.x + threadIdx.y * blockDim.x;
+    }
 }
 
 __global__ void kernel5( int *a, int dimx, int dimy )
 {
+    int ix   = blockIdx.x*blockDim.x + threadIdx.x;
+    int iy   = blockIdx.y*blockDim.y + threadIdx.y;
+    int idx = iy*dimx + ix;
+    if (ix < dimx && iy < dimy){
+        a[idx] = blockIdx.y;
+    }
 }
 
 __global__ void kernel6( int *a, int dimx, int dimy )
 {
+    int ix   = blockIdx.x*blockDim.x + threadIdx.x;
+    int iy   = blockIdx.y*blockDim.y + threadIdx.y;
+    int idx = iy*dimx + ix;
+    if (ix < dimx && iy < dimy){
+        a[idx] = blockIdx.x;
+    }
 }
 
 
